ddc422683e5ddfa929b1767071cdbc3007a97b84
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2021-10-13 22:28:53 +0530

    Create use math header file.c

---
+++ use math header file.c
@@ -0,0 +1,11 @@
+//WAP to use maths.h
+#include<stdio.h>
+#include<math.h>
+int main(){
+	printf("%f  ",exp(log(5)));
+	printf("%f  ",log10(10));
+	printf("%f  ",exp(2));
+	printf("%f  ",sqrt(16));
+	printf("%f  ",pow(20,5));
+	return 0;
+}
