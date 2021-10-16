fa53025d535ef70160bd341d5c65d0b7bc1e3cdb
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2021-10-16 18:41:33 +0530

    Update and rename driving liecence to driving licence.c

---
+++ driving licence.c
@@ -0,0 +1,14 @@
+//program for a driving licence
+
+#include<stdio.h>
+int main(){
+	int age = 8;
+	if (age<18){
+		printf("You can't apply for driving licence");
+	}
+	else{
+		printf("you can apply for drivimg licence");
+	}
+	
+	return 0;
+}
