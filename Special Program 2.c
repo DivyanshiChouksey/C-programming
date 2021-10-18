922e8b45ec20116a74798093dc97d6bcda1711b7
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2021-10-18 22:38:58 +0530

    Create Special Program 2.c

---
+++ Special Program 2.c
@@ -0,0 +1,13 @@
+//Special Program 2
+#include<stdio.h>
+
+//while(1)
+int main(){
+	if (5,25,8,4,0) 
+		//the last element gets check for if condition
+		printf("hello");
+	else
+		printf("hi");
+		
+	return 0; 
+}
