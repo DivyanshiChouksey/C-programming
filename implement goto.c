99684ea2c9d64bcaf3b742e0aaa0237fad6fe12a
DivyanshiChouksey <89761026+DivyanshiChouksey@users.noreply.github.com>
2022-01-28 21:43:02 +0530

    Update implement goto.c

--- implement goto.c
+++ implement goto.c
@@ -3,13 +3,13 @@
 int main(){
 	for(int i=1;i<=10;i++){
 		if (i==3){
-			printf("your num has 3:not allowed\n");
+			printf("Your num has 3:not allowed\n");
 			goto abc;
 		}
 		printf("%d ",i);
 	}
-	printf("we are out of the loop ");
+	printf("We are out of the loop ");
 	abc:
-		printf("hey we are at abc ");	
+		printf("Hey we are at abc ");	
 	return 0;	
 }
